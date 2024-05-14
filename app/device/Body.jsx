8263c235ad9ad4accef1b37335893ab4a2f4cccb
mwang5 <mark.wang2@activenetwork.com>
2024-05-14 15:32:11 +0800

    use nextjs server render feature

---
+++ app/device/Body.jsx
@@ -0,0 +1,94 @@
+"use client"
+
+import React, { useEffect } from 'react'
+import { CloseOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Space, Typography } from 'antd'
+import { createDevice } from '@/lib/actions'
+
+const Body = ({data}) => {
+  const [form] = Form.useForm()
+  const onFinish = async (values) => {
+    await createDevice(values.devices)
+  }
+
+  useEffect(() => {
+
+    form.setFieldsValue(data);
+}, [data]);
+
+  return (
+    <Form
+      labelCol={{
+        span: 6,
+      }}
+      wrapperCol={{
+        span: 18,
+      }}
+      form={form}
+      name="dynamic_form_complex"
+      style={{
+        maxWidth: 600,
+      }}
+      autoComplete="off"
+      onFinish={onFinish}
+    >
+      <Form.Item label="List">
+        <Form.List name="devices">
+          {(subFields, subOpt) => (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                rowGap: 16,
+              }}
+            >
+              {subFields.map((subField) => (
+                <Space key={subField.key}>
+                  <Form.Item noStyle name={[subField.name, 'name']}>
+                    <Input placeholder="Device Name"/>
+                  </Form.Item>
+                  <Form.Item noStyle name={[subField.name, 'viewPort']}>
+                    <Input placeholder="Min. ViewPort"/>
+                  </Form.Item>
+                  <CloseOutlined
+                    onClick={() => {
+                      subOpt.remove(subField.name)
+                    }}
+                  />
+                </Space>
+              ))}
+              <Button type="dashed" onClick={() => subOpt.add()}
+                      block>
+                + Add Sub Item
+              </Button>
+            </div>
+          )}
+        </Form.List>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Save
+        </Button>
+
+        <Button type="default" htmlType="submit">
+          Discard
+        </Button>
+      </Form.Item>
+
+      <Form.Item noStyle shouldUpdate>
+        {() => (
+          <Typography>
+            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+          </Typography>
+        )}
+      </Form.Item>
+    </Form>
+  )
+}
+export default Body
--- app/device/page.js
+++ app/device/page.js
@@ -1,98 +1,19 @@
-"use client"
+import React  from 'react'
+import Body from '@/app/device/Body'
 
-import React, { useEffect } from 'react'
-import useSWR from "swr"
-import { CloseOutlined } from '@ant-design/icons'
-import { Button, Form, Input, Space, Typography } from 'antd'
-import { createDevice } from '@/lib/actions'
-
-const App = () => {
-  const [form] = Form.useForm()
-  const fetcher = (url) => fetch(url).then((r) => r.json());
-  const { data } = useSWR('/api/devices', fetcher)
-
-  const onFinish = async (values) => {
-    await createDevice(values.devices)
+async function getData() {
+  const res = await fetch('http://localhost:3001/api/devices')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
   }
 
-  useEffect(() => {
+  return res.json()
+}
 
-    form.setFieldsValue(data);
-}, [data]);
-
+const App = async () => {
+  const data = await getData()
   return (
-    <Form
-      labelCol={{
-        span: 6,
-      }}
-      wrapperCol={{
-        span: 18,
-      }}
-      form={form}
-      name="dynamic_form_complex"
-      style={{
-        maxWidth: 600,
-      }}
-      autoComplete="off"
-      onFinish={onFinish}
-    >
-      <Form.Item label="List">
-        <Form.List name="devices">
-          {(subFields, subOpt) => (
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                rowGap: 16,
-              }}
-            >
-              {subFields.map((subField) => (
-                <Space key={subField.key}>
-                  <Form.Item noStyle name={[subField.name, 'name']}>
-                    <Input placeholder="Device Name"/>
-                  </Form.Item>
-                  <Form.Item noStyle name={[subField.name, 'viewPort']}>
-                    <Input placeholder="Min. ViewPort"/>
-                  </Form.Item>
-                  <CloseOutlined
-                    onClick={() => {
-                      subOpt.remove(subField.name)
-                    }}
-                  />
-                </Space>
-              ))}
-              <Button type="dashed" onClick={() => subOpt.add()}
-                      block>
-                + Add Sub Item
-              </Button>
-            </div>
-          )}
-        </Form.List>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Save
-        </Button>
-
-        <Button type="default" htmlType="submit">
-          Discard
-        </Button>
-      </Form.Item>
-
-      <Form.Item noStyle shouldUpdate>
-        {() => (
-          <Typography>
-            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
-          </Typography>
-        )}
-      </Form.Item>
-    </Form>
+     <main><Body data={data} /></main>
   )
 }
 export default App
